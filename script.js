a19b90503a5423938a1af3a95863814c3d801e6e
ZYEYT <71366015+ZYEYT@users.noreply.github.com>
2022-06-19 13:57:22 +0800

    background sliderr
    pt 6

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const colorBtn = document.querySelector('.colorBtn');
-const bodyBcg = document.querySelector('body');
-const colors =['yellow','red','green','#3b5998'];
-colorBtn.addEventListener('click',changeColor);
-function changeColor(){
-    // bodyBcg.style.backgroudColor = colors{2};
-    let random =Math.floor(Math.random()*colors.length)
-    bodyBcg.style.background=colors[random]
+const nextBTn = document.querySelector('.nextBTn');
+const prevBTn = document.querySelector('.prevBTn');
+const container = document.querySelector('.images');
+
+let counter = 0;
+
+nextBTn.addEventListener('click',nextSlide);
+prevBTn.addEventListener('click',prevSlide);
+
+function nextSlide(){
+container.animate([{opacity:'0.1'},{opacity :'1.0'}],{duration:1000,fill:'forwards'});
+
+counter++;
+if(counter === 3){
+    counter = 0;
+}
+container.style.backgroundImage = `url(img/bcg-${counter}.jpg`
+}
+
+function prevSlide(){
+    container.animate([{opacity:'0.1'},{opacity :'1.0'}],{duration:1000,fill:'forwards'});   
+    
+
+    counter--;
+    if(counter === -1){
+        counter = 2;
+    }
+
+container.style.backgroundImage = `url(img/bcg-${counter}.jpg`
 }
